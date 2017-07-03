685875ab226e29c16a9447c96f96945ebff45b2f
antklim <antklim@gmail.com>
2017-07-03 22:49:23 +1000

    initial commit

---
+++ handler_test.js
@@ -0,0 +1,10 @@
+const handler = require('./handler')
+
+describe('OAuth handler for Slack', function() {
+
+  describe('main', () => {
+    it('should handle request with `code` parameter in body and send OAuth request to Slack')
+    it('should return error callback when `code` parameter not found in body')
+  })
+
+})
